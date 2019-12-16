79e676de7f3cd5b54dfa6cb684441e37958a3b6f
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-12-16 21:12:08 +0300

    (TO-2) Добавил класс factory

---
+++ MapSmartObjects/Factory.swift
@@ -0,0 +1,13 @@
+//
+//  Factory.swift
+//  MapSmartObjects
+//
+//  Created by Kirill Fedorov on 16.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import Foundation
+
+final class Factory
+{
+}
