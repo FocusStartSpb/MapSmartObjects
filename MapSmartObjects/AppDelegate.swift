912fb8585b0f5559a89c9fe9dce68740395a18bd
FocusStart1 <focusstart1@MacBook-Air-FocusStart1.local>
2019-12-16 11:42:00 +0300

    (A-1) Скопировал начальный проект из репозитория b0sya

---
+++ MapSmartObjects/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MapSmartObjects
+//
+//  Created by Максим Шалашников on 15.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate
+{
+	var window: UIWindow?
+
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		self.window = UIWindow(frame: UIScreen.main.bounds)
+		self.window?.rootViewController = UIViewController()
+		self.window?.makeKeyAndVisible()
+		return true
+	}
+}
---
+++ MapSmartObjectsTests/MapSmartObjectsTests.swift
@@ -0,0 +1,14 @@
+//
+//  MapSmartObjectsTests.swift
+//  MapSmartObjectsTests
+//
+//  Created by Максим Шалашников on 15.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import XCTest
+@testable import MapSmartObjects
+
+final class MapSmartObjectsTests: XCTestCase
+{
+}
