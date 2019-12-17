af8370c737c3e00d52e1945c9b5dea7582eab67f
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-12-17 19:49:37 +0300

    (TO-5) Создал модуль PinList

---
+++ MapSmartObjects/PinList/PinListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PinListViewController.swift
+//  MapSmartObjects
+//
+//  Created by Максим Шалашников on 17.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import UIKit
+
+final class PinListViewController: UIViewController
+{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
