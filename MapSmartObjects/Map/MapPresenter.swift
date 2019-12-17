aafaf69c3496696137492be9a557b673aa5f6c6d
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-12-17 23:29:36 +0300

    (ТО-4) добавил модуль Map

---
+++ MapSmartObjects/Map/MapPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  MapPresenter.swift
+//  MapSmartObjects
+//
+//  Created by Максим Шалашников on 17.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import Foundation
+
+protocol IMapPresenter
+{
+}
+
+final class MapPresenter
+{
+}
---
+++ MapSmartObjects/Map/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  MapSmartObjects
+//
+//  Created by Максим Шалашников on 17.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController
+{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
