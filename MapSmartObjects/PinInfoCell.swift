505fb07429d3ebd1562f855a9431bedb1aecbb8b
Igor Shelginskiy <engwar@yandex.ru>
2019-12-17 20:16:29 +0300

    (ТО-8) исправление замечаний

--- MapSmartObjects/PinInfoCell.swift
+++ MapSmartObjects/PinInfoCell.swift
@@ -16,14 +16,12 @@ final class PinInfoCell: UITableViewCell
 		let label = UILabel()
 		label.font = UIFont.boldSystemFont(ofSize: 16)
 		label.textAlignment = .left
-		label.numberOfLines = 0
 		return label
 	}()
 	private let descriptionLabel: UILabel = {
 		let label = UILabel()
 		label.font = UIFont.systemFont(ofSize: 14)
 		label.textAlignment = .left
-		label.numberOfLines = 0
 		return label
 	}()
 
@@ -47,29 +45,18 @@ final class PinInfoCell: UITableViewCell
 	}
 
 	private func setConstraints() {
-		// Name label constraints
+		// Name & Description labels constraints
 		titleLabel.translatesAutoresizingMaskIntoConstraints = false
-		titleLabel.leadingAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
-		titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
-		titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30).isActive = true
-		titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
-		// Description label constraints
 		descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-		descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2).isActive = true
-		descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
-		descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor,
-												   constant: -30).isActive = true
-		descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: readableContentGuide.bottomAnchor,
-												 constant: -8).isActive = true
-		let descriptionLabelBottomAnchor = NSLayoutConstraint(
-			item: descriptionLabel,
-			attribute: .bottom,
-			relatedBy: .equal,
-			toItem: self,
-			attribute: .bottom,
-			multiplier: 1,
-			constant: -8)
-		descriptionLabelBottomAnchor.priority = .defaultLow
-		descriptionLabelBottomAnchor.isActive = true
+		NSLayoutConstraint.activate([
+			titleLabel.leadingAnchor.constraint(equalTo: leftAnchor, constant: 8),
+			titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+			titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+			titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+			descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2),
+			descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+			descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -30),
+			descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: readableContentGuide.bottomAnchor, constant: -8),
+			])
 	}
 }
