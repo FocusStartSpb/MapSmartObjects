6f1e27a9db66df474fe6acbbe76d054f39455597
Igor Shelginskiy <engwar@yandex.ru>
2019-12-18 21:11:33 +0300

    (ТО-7) добавил UI элементы в PinListController

--- MapSmartObjects/PinList/PinListViewController.swift
+++ MapSmartObjects/PinList/PinListViewController.swift
@@ -10,7 +10,48 @@ import UIKit
 
 final class PinListViewController: UIViewController
 {
+	private let pinTableView = UITableView()
+	private var pins = [AnyObject]()
+	private let presenter: IPinListPresenter
+	private let cellID = "pin"
+
+	init(presenter: IPinListPresenter) {
+		self.presenter = presenter
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.addSubview(pinTableView)
+		pinTableView.dataSource = self
+	}
+
+	private func setConstraint() {
+		pinTableView.translatesAutoresizingMaskIntoConstraints = true
+		NSLayoutConstraint.activate([
+			pinTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+			pinTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+			pinTableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+			pinTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+		])
+	}
+}
+
+extension PinListViewController: UITableViewDelegate, UITableViewDataSource
+{
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return pins.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? PinInfoCell
+		else { return UITableViewCell() }
+		//реализация ячейки
+		return cell
 	}
 }
