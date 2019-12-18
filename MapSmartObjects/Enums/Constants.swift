b4d2213508cff15af0ff37d2ab401f529c094a5d
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-12-18 20:11:13 +0300

    (TO-17) создать yandexGeocoder

---
+++ MapSmartObjects/Enums/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MapSmartObjects
+//
+//  Created by Kirill Fedorov on 18.12.2019.
+//  Copyright © 2019 Максим Шалашников. All rights reserved.
+//
+
+import Foundation
+
+enum Constants
+{
+	static let apiKey = "0bd25207-2244-4998-a106-6125d7b85900"
+	static let baseUrl = "https://geocode-maps.yandex.ru/1.x"
+}
