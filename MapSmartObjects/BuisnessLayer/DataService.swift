0e099c22b2d6ff6b60d3fb2f33ea0ce04a5b01ca
Igor Shelginskiy <engwar@yandex.ru>
2019-12-19 19:53:05 +0300

    (ТО-16) Исправление замечаний

--- MapSmartObjects/BuisnessLayer/DataService.swift
+++ MapSmartObjects/BuisnessLayer/DataService.swift
@@ -14,21 +14,21 @@ final class DataService
 	var pins = [GeocoderResponse]()
 
 	func saveFile() {
-		let documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-		let archURL = documentDir?.appendingPathComponent("data").appendingPathExtension("plist") ??
-			documentDir.unsafelyUnwrapped
-		let propListEnc = PropertyListEncoder()
-		let encodedCars = try? propListEnc.encode(pins)
-		try? encodedCars?.write(to: archURL, options: .noFileProtection)
+		let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+		let archiveURL = documentDirectory?.appendingPathComponent("data").appendingPathExtension("plist") ??
+			documentDirectory.unsafelyUnwrapped
+		let pinListEncoder = PropertyListEncoder()
+		let encodedPins = try? pinListEncoder.encode(pins)
+		try? encodedPins?.write(to: archiveURL, options: .noFileProtection)
 	}
 
 	func loadFile() {
-		let documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-		let archURL = documentDir?.appendingPathComponent("data").appendingPathExtension("plist") ??
-			documentDir.unsafelyUnwrapped
-		guard let data = try? Data(contentsOf: archURL) else { return }
-		let propListDec = PropertyListDecoder()
-		guard let pins = try? propListDec.decode([GeocoderResponse].self, from: data) else { return }
+		let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+		let archiveURL = documentDirectory?.appendingPathComponent("data").appendingPathExtension("plist") ??
+			documentDirectory.unsafelyUnwrapped
+		guard let data = try? Data(contentsOf: archiveURL) else { return }
+		let pinListDecoder = PropertyListDecoder()
+		guard let pins = try? pinListDecoder.decode([GeocoderResponse].self, from: data) else { return }
 		self.pins = pins
 	}
 }
