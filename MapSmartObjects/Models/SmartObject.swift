724caf1f3a31a7c8e25a8142d809a3dae4cf669c
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-12-19 20:38:42 +0300

    (TO-26) Подписал на протокол Codable

--- MapSmartObjects/Models/SmartObject.swift
+++ MapSmartObjects/Models/SmartObject.swift
@@ -9,17 +9,19 @@
 import Foundation
 import MapKit
 
-final class SmartObject
+final class SmartObject: Codable
 {
 	var name: String
 	var address: String
-	var coordinate: CLLocationCoordinate2D
+	var latitude: Double
+	var longitude: Double
 	var circleRadius: Double
 
-	init(name: String, address: String, coordinate: CLLocationCoordinate2D, circleRadius: Double) {
+	init(name: String, address: String, latitude: Double, longitude: Double, circleRadius: Double) {
 		self.name = name
 		self.address = address
-		self.coordinate = coordinate
+		self.latitude = latitude
+		self.longitude = longitude
 		self.circleRadius = circleRadius
 	}
 }
