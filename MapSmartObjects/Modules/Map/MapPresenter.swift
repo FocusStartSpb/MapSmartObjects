fc70f5781a284311b0622e92e38cac4ff00e2bb8
Igor Shelginskiy <engwar@yandex.ru>
2020-01-04 13:55:39 +0300

    (ТО-52) поддержка айпада

--- MapSmartObjects/Modules/Map/MapPresenter.swift
+++ MapSmartObjects/Modules/Map/MapPresenter.swift
@@ -75,7 +75,7 @@ extension MapPresenter: IMapPresenter
 
 	private func ckeckAutorization() {
 		switch CLLocationManager.authorizationStatus() {
-		case.authorizedWhenInUse, .authorizedAlways, .notDetermined:
+		case .authorizedWhenInUse, .authorizedAlways, .notDetermined:
 			mapViewController?.getLocationManager().requestAlwaysAuthorization()
 		case .denied, .restricted:
 			mapViewController?.showAlertLocation(title: "You have banned the use of location",
