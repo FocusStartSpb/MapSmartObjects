6c410dbc6dba1578d21ed8812990c8e5cffa04ef
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2020-01-10 17:37:31 +0300

    (ТО-63) Добавил иконки в tabbar и изменил иконки в map модуле

--- MapSmartObjects/Modules/MainTabBarController.swift
+++ MapSmartObjects/Modules/MainTabBarController.swift
@@ -11,6 +11,7 @@ import UIKit
 final class MainTabBarController: UITabBarController
 {
 	private let factory = Factory()
+	private let imageInset = UIEdgeInsets(top: 30.0, left: 30.0, bottom: 30.0, right: 30.0)
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,8 +21,11 @@ final class MainTabBarController: UITabBarController
 		self.addChild(mapController)
 		self.addChild(pinListController)
 
-		mapController.tabBarItem = UITabBarItem(title: "Map", image: nil, tag: 1)
-		pinListController.tabBarItem = UITabBarItem(title: "My Pins", image: nil, tag: 2)
+		mapController.tabBarItem = UITabBarItem(title: "Map", image: UIImage(named: "map"), tag: 1)
+		mapController.tabBarItem.imageInsets = imageInset
+
+		pinListController.tabBarItem = UITabBarItem(title: "My Pins", image: UIImage(named: "menu"), tag: 2)
+		pinListController.tabBarItem.imageInsets = imageInset
 	}
 
 	init() {
