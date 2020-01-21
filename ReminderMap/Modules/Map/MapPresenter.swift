f84b7bc8fbef2efc9c8899d96cab2a26f08601d1
Кирилл Федоров <ikirillfedorov@gmail.com>
2020-01-21 20:50:51 +0300

    (ТО-104) Ввел дополнительную установку во viewWillAppear

--- ReminderMap/Modules/Map/MapPresenter.swift
+++ ReminderMap/Modules/Map/MapPresenter.swift
@@ -58,7 +58,7 @@ final class MapPresenter: NSObject
 				let smartObject = SmartObject(name: name, address: position, coordinate: coordinate, circleRadius: maxRadius)
 				DispatchQueue.main.async {
 					self.mapViewController?.hideActivityIndicator()
-					self.mapViewController?.setMonitoringPlacesCount()
+					self.startMonitoring(smartObject)
 					self.router.showDetails(smartObject: smartObject, type: .create)
 				}
 			case .failure(let error):
